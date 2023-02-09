ea80e0a2f210e32751784bea3487032cc5c0ea5d
Subham Kr Gupta <subham.gupta@motivitylabs.com>
2023-02-09 15:56:57 +0530

    Authentication JWT : add AuthenticationManager and get AuthenticationManager using AuthenticationConfiguration

--- RESTAPIWithAuthenticationJWT/src/main/java/com/spring/security/controller/UserInfoController.java
+++ RESTAPIWithAuthenticationJWT/src/main/java/com/spring/security/controller/UserInfoController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,7 +65,19 @@ public class UserInfoController {
 	
 	@PostMapping("/auth")
 	public String authenticationAndGetToken(@RequestBody AuthRequest authRequest) {
-		return jwtService.generateToken(authRequest.getUsername());
+		
+		Authentication authenticate = authenticationManager.authenticate(new 
+				UsernamePasswordAuthenticationToken(authRequest.getUsername()
+						, authRequest.getPassword()));
+		
+		if(authenticate.isAuthenticated()) {
+			return jwtService.generateToken(authRequest.getUsername());
+		}
+		else {
+			throw new UsernameNotFoundException("Invalid user Request !");
+		}
+		
+		
 	}
 	
 	
